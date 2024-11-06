de08683f7f746b9c9ac11ccc8570cc0fc20df6bf
iamismail <ismailmbarouk@gmail.com>
2024-11-06 17:30:16 +0300

    variables

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 // This is my first JavaScript code!
-console.log("hello wold");
+// console.log("hello wold");
+
+// variables
+// let name = 'ismail';
+// console.log(name);
+
+// let firstName = 'ismail';
+// let lastName = 'Said';
+
+// let name = 'ismail'; // string literal
+// let age = 23; // Number literal
+// let isApproved = true; // boolean literal
+// let firstName = undefined; 
+// let selectedColor = null; 
+
+let person = {
+    name: 'ismail',
+    age: 23,
+};
+
+
+person.name = 'juma';
+
+
+let selectedColor = ['red', 'blue']
+selectedColor[2] = 'pink';
+console.log(selectedColor.length);
